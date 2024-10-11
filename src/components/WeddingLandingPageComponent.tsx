dd4a208806149a246837c601263373b7773a5f35
G-Fidalgo <gfidalgo@smartclipspain.com>
2024-10-11 15:30:52 +0200

    refactor(landing): update components & formats

--- src/components/WeddingLandingPageComponent.tsx
+++ src/components/WeddingLandingPageComponent.tsx
@@ -2,16 +2,20 @@ import { useState, useEffect, useRef } from "react";
 import { HeroSection } from "./sections/HeroSection";
 import { CountdownSection } from "./sections/CountdownSection";
 import { WeddingInfoSection } from "./sections/WeddingInfoSection";
-import { WeatherSection } from "./sections/WeatherSection";
 import { GiftsSection } from "./sections/GiftsSection";
 import { BankAccountComponent } from "./bank-account";
-import { BusInfoSection } from "./sections/BusInfoSection";
 import { HeaderComponent } from "./sections/HeaderComponent";
+import WeatherCarousel, { WeatherData } from "./sections/WeatherSection";
 
 export function WeddingLandingPageComponent() {
   const refCountDownSection = useRef<HTMLDivElement | null>(null);
   const refChevron = useRef<HTMLButtonElement | null>(null);
-  const [countdown, setCountdown] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+  const [countdown, setCountdown] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
   const weddingDate = new Date("2025-06-21T17:30:00");
   const [isSticky, setIsSticky] = useState(false);
 
@@ -34,7 +38,7 @@ export function WeddingLandingPageComponent() {
     const handleScroll = () => {
       if (refChevron.current) {
         const heroBottom =
-        refChevron.current.offsetTop + refChevron.current.offsetHeight - 32;
+          refChevron.current.offsetTop + refChevron.current.offsetHeight - 32;
         setIsSticky(window.pageYOffset > heroBottom);
       }
     };
@@ -56,80 +60,98 @@ export function WeddingLandingPageComponent() {
     {
       id: 1,
       title: "Set de Cocina",
-      image: "https://binamehta.com/wp-content/uploads/image-placeholder-300x200.png?height=200&width=300",
+      image:
+        "https://binamehta.com/wp-content/uploads/image-placeholder-300x200.png?height=200&width=300",
       reserved: false,
     },
     {
       id: 2,
       title: "Viaje a París",
-      image: "https://binamehta.com/wp-content/uploads/image-placeholder-300x200.png?height=200&width=300",
+      image:
+        "https://binamehta.com/wp-content/uploads/image-placeholder-300x200.png?height=200&width=300",
       reserved: true,
     },
     {
       id: 3,
       title: "Juego de Sábanas",
-      image: "https://binamehta.com/wp-content/uploads/image-placeholder-300x200.png?height=200&width=300",
+      image:
+        "https://binamehta.com/wp-content/uploads/image-placeholder-300x200.png?height=200&width=300",
       reserved: false,
     },
     {
       id: 4,
       title: "Cena Romántica",
-      image: "https://binamehta.com/wp-content/uploads/image-placeholder-300x200.png?height=200&width=300",
+      image:
+        "https://binamehta.com/wp-content/uploads/image-placeholder-300x200.png?height=200&width=300",
       reserved: false,
     },
     {
       id: 5,
       title: "Vajilla Completa",
-      image: "https://binamehta.com/wp-content/uploads/image-placeholder-300x200.png?height=200&width=300",
+      image:
+        "https://binamehta.com/wp-content/uploads/image-placeholder-300x200.png?height=200&width=300",
       reserved: true,
     },
     {
       id: 6,
       title: "Curso de Cocina",
-      image: "https://binamehta.com/wp-content/uploads/image-placeholder-300x200.png?height=200&width=300",
+      image:
+        "https://binamehta.com/wp-content/uploads/image-placeholder-300x200.png?height=200&width=300",
       reserved: false,
     },
   ];
 
-  const weatherData = [
-    { time: "00:00", temperature: 10, precipitation: 0, sunHours: 0 },
-    { time: "02:00", temperature: 9, precipitation: 0, sunHours: 0 },
-    { time: "04:00", temperature: 9, precipitation: 0, sunHours: 0 },
-    { time: "06:00", temperature: 10, precipitation: 0, sunHours: 1 },
-    { time: "08:00", temperature: 13, precipitation: 0, sunHours: 2 },
-    { time: "10:00", temperature: 16, precipitation: 0, sunHours: 3 },
-    { time: "12:00", temperature: 18, precipitation: 0, sunHours: 4 },
-    { time: "14:00", temperature: 21, precipitation: 0, sunHours: 4 },
-    { time: "16:00", temperature: 22, precipitation: 0, sunHours: 4 },
-    { time: "18:00", temperature: 21, precipitation: 0, sunHours: 3 },
-    { time: "20:00", temperature: 18, precipitation: 0, sunHours: 2 },
-    { time: "22:00", temperature: 15, precipitation: 0, sunHours: 0 },
+  const weatherData: WeatherData[] = [
+    { time: "00:00", temperature: 10, condition: "clearNight" },
+    { time: "01:00", temperature: 10, condition: "clearNight" },
+    { time: "02:00", temperature: 9, condition: "clearNight" },
+    { time: "03:00", temperature: 9, condition: "clearNight" },
+    { time: "04:00", temperature: 9, condition: "clearNight" },
+    { time: "05:00", temperature: 9, condition: "clearNight" },
+    { time: "06:00", temperature: 10, condition: "sunny" },
+    { time: "06:45", temperature: 10, condition: "sunrise" },
+    { time: "07:00", temperature: 13, condition: "sunny" },
+    { time: "08:00", temperature: 13, condition: "sunny" },
+    { time: "10:00", temperature: 16, condition: "sunny" },
+    { time: "12:00", temperature: 18, condition: "sunny" },
+    { time: "14:00", temperature: 21, condition: "sunny" },
+    { time: "15:00", temperature: 21, condition: "sunny" },
+    { time: "16:00", temperature: 22, condition: "sunny" },
+    { time: "17:00", temperature: 22, condition: "sunny" },
+    { time: "18:00", temperature: 21, condition: "sunny" },
+    { time: "19:00", temperature: 21, condition: "sunny" },
+    { time: "20:00", temperature: 18, condition: "sunny" },
+    { time: "21:00", temperature: 18, condition: "sunny" },
+    { time: "21:49", temperature: 10, condition: "sunset" },
+    { time: "22:00", temperature: 15, condition: "clearNight" },
+    { time: "23:00", temperature: 15, condition: "clearNight" },
   ];
 
-
   return (
-    <div className="min-h-screen bg-gray-100">
-      <HeaderComponent isSticky={isSticky}/>
-      
-      <HeroSection scrollToNextSection={scrollToNextSection}
-       refChevronSection={refChevron} />
+    <div className="min-h-screen  ">
+      <HeaderComponent isSticky={isSticky} />
 
-      <CountdownSection countdown={countdown} refCountDownSection={refCountDownSection} />
+      <HeroSection
+        scrollToNextSection={scrollToNextSection}
+        refChevronSection={refChevron}
+      />
+      <div className="max-w-[80ch] mx-auto">
+        <CountdownSection
+          countdown={countdown}
+          refCountDownSection={refCountDownSection}
+        />
 
-      <WeddingInfoSection />
+        <WeddingInfoSection />
 
-      <WeatherSection weatherData={weatherData} />
+        <WeatherCarousel weatherData={weatherData} />
+      </div>
 
       {/* Ubicaciones Section */}
-
-      <BusInfoSection/>
 
       <section className="bg-white">
         <BankAccountComponent />
         <GiftsSection gifts={gifts} />
       </section>
-
-      
     </div>
   );
 }
